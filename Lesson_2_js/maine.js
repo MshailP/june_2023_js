74bfebca6dbae44814774c730cc8e405b43ad571
Mykhailo_P <mihail.prohorov.02>
2023-08-19 10:50:43 +0300

    add arr 0-9 and array Users

--- Lesson_2_js/maine.js
+++ Lesson_2_js/maine.js
@@ -3,29 +3,62 @@
 // типу, вивести кожен елемент в консоль
 
 
+let arr = [];
+arr[0] = 1;
+arr[1] = 'hello';
+arr[2] = 'world';
+arr[3] = 'qwe';
+arr[4] = 513;
+arr[5] = 62948.34;
+arr[6] = 'v';
+arr[7] = -15;
+arr[8] = true;
+arr[9] = false;
+
+console.log(arr[3]);
+
+arr[3] = 'js';
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
 
 
-
-
-
-
-// - Створити 3 об'єкти console.log(var10);які описують книги. Поля об'єкту : title ,pageCount, genre.
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
 
 let book1 = {
-    title: 'story',
-    pageCount: 256,
-    genre: 'comedy',
+    title: 'story1',
+    pageCount: 617,
+    genre: 'fantasy',
+}
+let book2 = {
+    title: 'story2',
+    pageCount: 316,
+    genre: 'short stories',
+}
+let book3 = {
+    title: 'story3',
+    pageCount: 956,
+    genre: 'romance novel',
 }
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.
 
-let book = {
-    title: 'story',
-    pageCount: 256,
-    genre: 'comedy',
+
+let book1_1 = {
+    title: 'story1_1',
+    pageCount: 617,
+    genre: 'fantasy',
     authors: [
         {
-            name: 'Mihail',
+            name: 'Mykhailo',
             age: 22
         },
         {
@@ -39,8 +72,104 @@ let book = {
     ]
 }
 
+let book1_2 = {
+    title: 'story1_2',
+    pageCount: 316,
+    genre: 'short stories',
+    authors: [
+        {
+            name: 'Oleg',
+            age: 31
+        },
+        {
+            name: 'Rodion',
+            age: 24
+        },
+        {
+            name: 'Yuri',
+            age: 48
+        }
+    ]
+}
+
+let book1_3 = {
+    title: 'story1_3',
+    pageCount: 956,
+    genre: 'romance novel',
+    authors: [
+        {
+            name: 'Philip',
+            age: 35
+        },
+        {
+            name: 'Roman',
+            age: 29
+        },
+        {
+            name: 'Oksana',
+            age: 32
+        }
+    ]
+}
+
+
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
-//
+
+let Users = [
+    {
+        name:'Mykhailo',
+        username:'user145352443',
+        password: 375411
+    },
+    {
+        name:'Olexander',
+        username:'user23245235',
+        password: 444451
+    },
+    {
+        name:'Andriy',
+        username:'user32346345',
+        password: 333352
+    },
+    {
+        name:'Victor',
+        username:'user42345325',
+        password: 999932
+    },
+    {
+        name:'Glib',
+        username:'user5345234523',
+        password: 998034
+    },
+    {
+        name:'Evgen',
+        username:'user632453254',
+        password: 375460
+    },
+    {
+        name:'Ivan',
+        username:'user72345235',
+        password: 334213
+    },
+    {
+        name:'Kostyantyn',
+        username:'user82352323',
+        password: 300029
+    },
+    {
+        name:'Maxim',
+        username:'user923452345',
+        password: 123456
+    },
+    {
+        name:'Sergiy',
+        username:'user1023453245',
+        password: 654321
+    }
+]
+
+console.log(Users)
+
 //
 // Логічні розгалуження:
 //     - Є змінна х, якій ви надаєте довільне числове значення.
